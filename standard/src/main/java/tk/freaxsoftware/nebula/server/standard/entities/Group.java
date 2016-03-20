e82e699ec25865a254c69a8071a52af5c27544e7
Stanislav Nepochatov <spoilt@blackstarfilm.com>
2016-03-21 01:34:21 +0200

    Add User and Group entities;

---
+++ standard/src/main/java/tk/freaxsoftware/nebula/server/standard/entities/Group.java
@@ -0,0 +1,196 @@
+/*
+ * This file is part of Nebula Server application.
+ * 
+ * Copyright (C) 2015 Freax Software
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+package tk.freaxsoftware.nebula.server.standard.entities;
+
+import java.util.Map;
+import java.util.Objects;
+import tk.freaxsoftware.extras.faststorage.generic.ECSVAble;
+import tk.freaxsoftware.extras.faststorage.generic.ECSVDefinition;
+import tk.freaxsoftware.extras.faststorage.generic.ECSVFields;
+import tk.freaxsoftware.extras.faststorage.generic.EntityListReference;
+import tk.freaxsoftware.extras.faststorage.reading.EntityReader;
+import tk.freaxsoftware.extras.faststorage.writing.EntityWriter;
+
+/**
+ * User group entity.
+ * @author Stanislav Nepochatov
+ */
+public class Group implements ECSVAble<String> {
+    
+    public static final String TYPE = "GROUP";
+    
+    public static final ECSVDefinition DEFINITION = ECSVDefinition.createNew()
+            .addKey(String.class)
+            .addPrimitive(ECSVFields.PR_STRING)
+            .addPrimitive(ECSVFields.PR_STRING)
+            .addReferenceArray(User.class, String.class)
+            .addMap(null, null);
+    
+    /**
+     * Group name.
+     */
+    private String name;
+    
+    /**
+     * Group short description.
+     */
+    private String description;
+    
+    /**
+     * Image url for group avatar.
+     */
+    private String imageUrl;
+    
+    /**
+     * Users of group.
+     */
+    private EntityListReference<User, String> users;
+    
+    /**
+     * Attribute map.
+     */
+    private Map<String, String> attrs;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public EntityListReference<User, String> getUsers() {
+        return users;
+    }
+
+    public void setUsers(EntityListReference<User, String> users) {
+        this.users = users;
+    }
+
+    public Map<String, String> getAttrs() {
+        return attrs;
+    }
+
+    public void setAttrs(Map<String, String> attrs) {
+        this.attrs = attrs;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Group other = (Group) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.imageUrl, other.imageUrl)) {
+            return false;
+        }
+        if (!Objects.equals(this.attrs, other.attrs)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" + "name=" + name + ", description=" + description + ", imageUrl=" + imageUrl + ", attrs=" + attrs + '}';
+    }
+
+    @Override
+    public String getEntityType() {
+        return TYPE;
+    }
+
+    @Override
+    public String getKey() {
+        return name;
+    }
+
+    @Override
+    public void setKey(String key) {
+        throw new IllegalStateException("This entity doesn't support setting key.");
+    }
+
+    @Override
+    public ECSVDefinition getDefinition() {
+        return DEFINITION;
+    }
+
+    @Override
+    public void readFromECSV(EntityReader<String> reader) {
+        this.name = reader.readKey();
+        this.description = reader.readString();
+        this.imageUrl = reader.readString();
+        this.users = reader.readReferenceArray(User.class);
+        this.attrs = reader.readMap();
+    }
+
+    @Override
+    public void writeToECSV(EntityWriter<String> writer) {
+        writer.writeKey(this.name);
+        writer.writeString(this.description);
+        writer.writeString(this.imageUrl);
+        writer.writeReferenceArray(users);
+        writer.writeMap(attrs);
+    }
+
+    @Override
+    public void update(ECSVAble<String> updatedEntity) {
+        if (updatedEntity instanceof Group) {
+            Group other = (Group) updatedEntity;
+            this.name = other.name;
+            this.description = other.description;
+            this.imageUrl = other.imageUrl;
+            this.users = other.users;
+            this.attrs = other.attrs;
+        }
+    }
+    
+}
---
+++ standard/src/main/java/tk/freaxsoftware/nebula/server/standard/entities/User.java
@@ -0,0 +1,278 @@
+/*
+ * This file is part of Nebula Server application.
+ * 
+ * Copyright (C) 2015 Freax Software
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+package tk.freaxsoftware.nebula.server.standard.entities;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
+import tk.freaxsoftware.extras.faststorage.generic.ECSVAble;
+import tk.freaxsoftware.extras.faststorage.generic.ECSVDefinition;
+import tk.freaxsoftware.extras.faststorage.generic.ECSVFields;
+import tk.freaxsoftware.extras.faststorage.generic.EntityListReference;
+import tk.freaxsoftware.extras.faststorage.reading.EntityReader;
+import tk.freaxsoftware.extras.faststorage.writing.EntityWriter;
+
+/**
+ * User entity.
+ * @author Stanislav Nepochatov
+ */
+public class User implements ECSVAble<String> {
+    
+    public static final String TYPE = "USER";
+    
+    public static final ECSVDefinition DEFINITION = ECSVDefinition.createNew()
+            .addKey(String.class)
+            .addPrimitive(ECSVFields.PR_WORD)
+            .addPrimitive(ECSVFields.PR_STRING)
+            .addPrimitive(ECSVFields.PR_STRING)
+            .addPrimitive(ECSVFields.PR_STRING)
+            .addPrimitive(ECSVFields.PR_WORD)
+            .addPrimitive(ECSVFields.PR_BOOLEAN)
+            .addDate("yyyyMMddHHmmssz")
+            .addReferenceArray(Group.class, String.class)
+            .addMap(null, null);
+    
+    /**
+     * User login.
+     */
+    private String login;
+    
+    /**
+     * User name.
+     */
+    private String username;
+    
+    /**
+     * Basic user description.
+     */
+    private String description;
+    
+    /**
+     * Image url for avatar.
+     */
+    private String imageUrl;
+    
+    /**
+     * Hashed password.
+     */
+    private String password;
+    
+    /**
+     * Active user or not.
+     */
+    private Boolean active;
+    
+    /**
+     * Date of user login expiration.
+     */
+    private Date expireDate;
+    
+    /**
+     * Groups of user.
+     */
+    private EntityListReference<Group, String> groups;
+    
+    /**
+     * Maps of additional attributes.
+     */
+    private Map<String, String> attrs;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean isActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Date getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(Date expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public EntityListReference<Group, String> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(EntityListReference<Group, String> groups) {
+        this.groups = groups;
+    }
+
+    public Map<String, String> getAttrs() {
+        return attrs;
+    }
+
+    public void setAttrs(Map<String, String> attrs) {
+        this.attrs = attrs;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 73 * hash + Objects.hashCode(this.login);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (!Objects.equals(this.login, other.login)) {
+            return false;
+        }
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.imageUrl, other.imageUrl)) {
+            return false;
+        }
+        if (!Objects.equals(this.password, other.password)) {
+            return false;
+        }
+        if (!Objects.equals(this.active, other.active)) {
+            return false;
+        }
+        if (!Objects.equals(this.expireDate, other.expireDate)) {
+            return false;
+        }
+        if (!Objects.equals(this.attrs, other.attrs)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "login=" + login + ", username=" + username + ", description=" + description + ", imageUrl=" + imageUrl + ", password=" + password + ", active=" + active + ", expireDate=" + expireDate + ", attrs=" + attrs + '}';
+    }
+
+    @Override
+    public String getEntityType() {
+        return TYPE;
+    }
+
+    @Override
+    public String getKey() {
+        return login;
+    }
+
+    @Override
+    public void setKey(String key) {
+        throw new IllegalStateException("This entity doesn't support setting key.");
+    }
+
+    @Override
+    public ECSVDefinition getDefinition() {
+        return DEFINITION;
+    }
+
+    @Override
+    public void readFromECSV(EntityReader<String> reader) {
+        this.login = reader.readKey();
+        this.username = reader.readString();
+        this.description = reader.readString();
+        this.imageUrl = reader.readString();
+        this.password = reader.readWord();
+        this.active = reader.readBoolean();
+        this.expireDate = reader.readDate();
+        this.groups = reader.readReferenceArray(Group.class);
+        this.attrs = reader.readMap();
+    }
+
+    @Override
+    public void writeToECSV(EntityWriter<String> writer) {
+        writer.writeKey(login);
+        writer.writeString(username);
+        writer.writeString(description);
+        writer.writeString(imageUrl);
+        writer.writeWord(password);
+        writer.writeBoolean(active);
+        writer.writeDate(expireDate);
+        writer.writeReferenceArray(groups);
+        writer.writeMap(attrs);
+    }
+
+    @Override
+    public void update(ECSVAble<String> updatedEntity) {
+        if (updatedEntity instanceof User) {
+            User other = (User) updatedEntity;
+            this.login = other.login;
+            this.username = other.username;
+            this.description = other.description;
+            this.imageUrl = other.imageUrl;
+            this.password = other.password;
+            this.active = other.active;
+            this.expireDate = other.expireDate;
+            this.groups = other.groups;
+            this.attrs = other.attrs;
+        }
+    }
+    
+}
